29b7647f7b1225c90e83969b07b3fb4f2876fb63
Anat0liy12 <59225033+Anat0liy12@users.noreply.github.com>
2020-07-21 17:04:52 +0300

    1

--- src/tolik.py
+++ src/tolik.py
@@ -1,2 +1,9 @@
 def congratulate_salary(message, name_list, salary, bonus):
+    if type(bonus)==type(float()):
+        b = bonus*salary
+    else:
+        b = bonus      
+    for a in name_list:
+        n = a.title()
+        print(message.title() + ' ' + n + salary + b)
     return
