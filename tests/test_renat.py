c5bb53df470b8c65e93b9661d732e3511176022c
Serhii A <aserhii@protonmail.com>
2020-07-21 12:39:54 +0300

    Fix

--- tests/test_renat.py
+++ tests/test_renat.py
@@ -2,4 +2,4 @@ from src.renat import salary_remind
 import pytest
 
 def test_simple_test():
-    assert salary_remind() is None
+    assert salary_remind(message=None, in_days=None, name_list=None) is None
--- tests/test_tolik.py
+++ tests/test_tolik.py
@@ -2,4 +2,4 @@ from src.tolik import congratulate_salary
 import pytest
 
 def test_simple_test():
-    assert congratulate_salary() is None
+    assert congratulate_salary(message=None, name_list=None, salary=None, bonus=None) is None
