69076fcf4824fe9b14c72787a79ff6f5698f8971
Anat0liy12 <59225033+Anat0liy12@users.noreply.github.com>
2020-07-21 22:31:12 +0300

    Update tolik.py

--- src/tolik.py
+++ src/tolik.py
@@ -5,9 +5,6 @@ def congratulate_salary(message, name_list, salary, bonus):
         b = bonus      
     for a in name_list:
         n = a.title()
-        f = str(salary + b)
-    if len(name_list) > 1:
-        lists = []
-    else     
+        d = int(salary + b)
+        f = str(d)
         return (message.capitalize() + ' ' + n + ' ' + f)
-print(congratulate_salary(message="Ваш доход", name_list=["Петр", "Иван"], salary=10000, bonus=2000))
