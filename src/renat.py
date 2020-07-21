1ab9ddf6615ac3595f62297fce32e61432da0cc9
RenatTOP <renatseverski@gmail.com>
2020-07-21 17:18:55 +0300

    Update renat.py

--- src/renat.py
+++ src/renat.py
@@ -4,5 +4,7 @@ def salary_remind(message, start_day, in_days, name_list):
     date = datetime.datetime.strptime(start_day, "%Y-%m-%d") + datetime.timedelta(days=in_days)
     for i in name_list:
         names = i.title()
-        print(message + ", " + names + ", " + date.strftime("%Y-%m-%d"))
+        print("\"" + message + ", " + names + ", " + date.strftime("%Y-%m-%d") + "\"")
     return
+
+salary_remind(message="Ваша зарплата", start_day="2020-07-21", in_days=1, name_list=["маша"])
